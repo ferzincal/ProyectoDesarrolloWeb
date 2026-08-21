4aee2295cb09f07fa09a25cfcbe2b1d4447ff3c8
ferzincal <ferzincal@gmail.com>
2026-08-21 17:37:32 -0600

    feat/ Configurar Next.js para Github Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
+  basePath: '/casa-aleluya',
+
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
